4aa12fe47373570b604e80d4e57353e1c1f32655
tarasov95 <lysy95@gmail.com>
2016-09-05 15:15:23 +0800

    support for web-socket based refresh

--- ajaxterm/src/main/java/org/kohsuke/ajaxterm/Session.java
+++ ajaxterm/src/main/java/org/kohsuke/ajaxterm/Session.java
@@ -8,6 +8,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.Writer;
+import java.util.Observable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -15,176 +16,193 @@ import java.util.logging.Logger;
  * Represents a session.
  *
  * <p>
- * A {@link Thread} is used to shuttle data back and force between the HTTP client
- * and the process that was forked. You can check the liveness of this thread to see
- * if the child process is still alive or not.
+ * A {@link Thread} is used to shuttle data back and force between the HTTP client and the process
+ * that was forked. You can check the liveness of this thread to see if the child process is still
+ * alive or not.
  *
  * @author Kohsuke Kawaguchi
  */
 public final class Session extends Thread {
-    private final ProcessWithPty childProcess;
 
-    private final Terminal terminal;
+	public Observable onOutput = new Observable() {
+		@Override
+		public void notifyObservers(Object terminal) {
+			if (this.countObservers() > 0) {
+				this.setChanged();
+				super.notifyObservers(terminal);
+			}
+		}
+	};
 
-    private final long time = System.currentTimeMillis();
+	private final ProcessWithPty childProcess;
 
-    /**
-     * When was this session accessed the last time?
-     */
-    private long lastAccess;
+	private final Terminal terminal;
 
-    private final Reader in;
-    private final Writer out;
+	private final long time = System.currentTimeMillis();
 
-    /**
-     * Creates a terminal session that pumps message between the child process and {@link Terminal}.
-     *
-     *
-     * @param width
-     *      Width of the terminal. For example, 80.
-     * @param height
-     *      Height of the terminal. For example, 25.
-     * @param terminal
-     *      Terminal name set to the TERM environment variable.
-     *      For the JavaScript terminal implemented in ajaxterm.js, specify the value taken from {@link #getAjaxTerm()}
-     * @param commands
-     *      Command line arguments of the process to launch.
-     *      {"/bin/bash","--login"} for example.
-     */
-    public Session(int width, int height, String terminal, String... commands) throws IOException {
-        this(width, height, new PtyProcessBuilder().commands(commands).env("TERM",terminal).forkWithHelper());
-    }
+	/**
+	 * When was this session accessed the last time?
+	 */
+	private long lastAccess;
 
-    /**
-     *
-     * @param width
-     *      Width of the terminal. For example, 80.
-     * @param height
-     *      Height of the terminal. For example, 25.
-     * @param childProcessWithTty
-     *      A child process forked with pty as its stdin/stdout.
-     *      Normally this needs to be created with {@link PtyProcessBuilder}.
-     *      Make sure to set the correct terminal name in its environment variable.
-     *
-     * @see PtyProcessBuilder
-     */
-    public Session(int width, int height, ProcessWithPty childProcessWithTty) throws IOException {
-        this.terminal = new Terminal(width,height);
-        this.childProcess = childProcessWithTty;
-        childProcess.setWindowSize(width,height);
+	private final Reader in;
+	private final Writer out;
 
-        in = new InputStreamReader(childProcess.getInputStream());
-        out = new OutputStreamWriter(childProcess.getOutputStream());
+	/**
+	 * Creates a terminal session that pumps message between the child process and {@link Terminal}.
+	 *
+	 *
+	 * @param width Width of the terminal. For example, 80.
+	 * @param height Height of the terminal. For example, 25.
+	 * @param terminal Terminal name set to the TERM environment variable. For the JavaScript terminal
+	 * implemented in ajaxterm.js, specify the value taken from {@link #getAjaxTerm()}
+	 * @param commands Command line arguments of the process to launch. {"/bin/bash","--login"} for
+	 * example.
+	 */
+	public Session(int width, int height, String terminal, String... commands) throws IOException {
+		this(width, height, new PtyProcessBuilder().commands(commands).env("TERM", terminal)
+						.forkWithHelper());
+	}
 
-        setName("Terminal pump thread for "+ childProcessWithTty);
-        start(); // start pumping
-    }
+	/**
+	 *
+	 * @param width Width of the terminal. For example, 80.
+	 * @param height Height of the terminal. For example, 25.
+	 * @param childProcessWithTty A child process forked with pty as its stdin/stdout. Normally this
+	 * needs to be created with {@link PtyProcessBuilder}. Make sure to set the correct terminal name
+	 * in its environment variable.
+	 *
+	 * @see PtyProcessBuilder
+	 */
+	public Session(int width, int height, ProcessWithPty childProcessWithTty) throws IOException {
+		this.terminal = new Terminal(width, height);
+		this.childProcess = childProcessWithTty;
+		childProcess.setWindowSize(width, height);
 
-    public Terminal getTerminal() {
-        return terminal;
-    }
+		in = new InputStreamReader(childProcess.getInputStream());
+		out = new OutputStreamWriter(childProcess.getOutputStream());
 
-    /**
-     * When was this session accessed by the client the last time?
-     */
-    public long getLastAccess() {
-        return lastAccess;
-    }
+		setName("Terminal pump thread for " + childProcessWithTty);
+		start(); // start pumping
+	}
 
-    /**
-     * When was this session allocated?
-     */
-    public long getTime() {
-        return time;
-    }
+	public Terminal getTerminal() {
+		return terminal;
+	}
 
-    @Override
-    public void run() {
-        char[] buf = new char[128];
-        int len;
+	/**
+	 * When was this session accessed by the client the last time?
+	 */
+	public long getLastAccess() {
+		return lastAccess;
+	}
 
-        try {
-            try {
-                while((len=in.read(buf))>=0) {
-                    terminal.write(new String(buf,0,len));
-                    String reply = terminal.read();
-                    if(reply!=null)
-                        out.write(reply);
-                }
-            } catch (IOException e) {
-                // fd created by forkpty seems to cause I/O error when the other side is closed via kill -9
-                if (!hasChildProcessFinished())
-                    LOGGER.log(Level.WARNING, "Session pump thread is dead", e);
-            } finally {
-                closeQuietly(in);
-                closeQuietly(out);
-            }
-        } catch (Throwable e) {
-            LOGGER.log(Level.WARNING, "Session pump thread is dead", e);
-        }
-    }
+	/**
+	 * When was this session allocated?
+	 */
+	public long getTime() {
+		return time;
+	}
 
-    private boolean hasChildProcessFinished() {
-        try {
-            childProcess.exitValue();
-            return true;
-        } catch (IllegalThreadStateException e) {
-            return false;
-        }
-    }
+	@Override
+	public void run() {
+		char[] buf = new char[128];
+		int len;
 
-    private void closeQuietly(Closeable c) {
-        try {
-            if (c!=null)    c.close();
-        } catch (IOException e) {
-            // silently ignore
-        }
-    }
+		try {
+			try {
+				while ((len = in.read(buf)) >= 0) {
+					terminal.write(new String(buf, 0, len));
+					if (len > 0) {
+						onOutput.notifyObservers(terminal);
+					}
+					String reply = terminal.read();
+					if (reply != null) {
+						out.write(reply);
+					}
+				}
+			} catch (IOException e) {
+				// fd created by forkpty seems to cause I/O error when the other side is closed via kill -9
+				if (!hasChildProcessFinished()) {
+					LOGGER.log(Level.WARNING, "Session pump thread is dead", e);
+				}
+			} finally {
+				closeQuietly(in);
+				closeQuietly(out);
+			}
+		} catch (Throwable e) {
+			LOGGER.log(Level.WARNING, "Session pump thread is dead", e);
+		}
+	}
 
-    /**
-     * Receives the call from the client-side JavaScript.
-     */
-    public void handleUpdate(HttpServletRequest req, HttpServletResponse rsp) throws IOException, InterruptedException {
-        handleUpdate(
-                req.getParameter("k"),
-                req.getParameter("c") != null,
-                Integer.parseInt(req.getParameter("t"))).renderResponse(rsp);
-    }
+	private boolean hasChildProcessFinished() {
+		try {
+			childProcess.exitValue();
+			return true;
+		} catch (IllegalThreadStateException e) {
+			return false;
+		}
+	}
 
-    /**
-     * Receives the call from the client-side JavaScript.
-     */
-    public ScreenImage handleUpdate(String keys, boolean color, int clientTimestamp) throws IOException, InterruptedException {
-        lastAccess = System.currentTimeMillis();
-        write(keys);
-        Thread.sleep(20);   // give a bit of time to let the app respond. poor version of Nagel's algorithm
+	private void closeQuietly(Closeable c) {
+		try {
+			if (c != null) {
+				c.close();
+			}
+		} catch (IOException e) {
+			// silently ignore
+		}
+	}
 
-        terminal.setCssClass(isAlive() ? "":"dead");
-        return terminal.dumpHtml(color,clientTimestamp);
-    }
+	/**
+	 * Receives the call from the client-side JavaScript.
+	 */
+	public void handleUpdate(HttpServletRequest req, HttpServletResponse rsp) throws IOException, InterruptedException {
+		handleUpdate(
+						req.getParameter("k"),
+						req.getParameter("c") != null,
+						Integer.parseInt(req.getParameter("t"))).renderResponse(rsp);
+	}
 
-    /**
-     * Write to the child process.
-     */
-    public void write(String k) throws IOException {
-        if(k!=null && k.length()!=0) {
-            out.write(k);
-            out.flush();
-        }
-    }
+	/**
+	 * Receives the call from the client-side JavaScript.
+	 */
+	public ScreenImage handleUpdate(String keys, boolean color, int clientTimestamp) throws IOException, InterruptedException {
+		lastAccess = System.currentTimeMillis();
+		write(keys);
+		Thread.sleep(20);   // give a bit of time to let the app respond. poor version of Nagel's algorithm
 
-    public Process getChildProcess() {
-        return childProcess;
-    }
+		terminal.setCssClass(isAlive() ? "" : "dead");
+		return terminal.dumpHtml(color, clientTimestamp);
+	}
 
-    /**
-     * Name of the terminal ajaxterm.js is implmenting.
-     *
-     * A static method instead of a constant to avoid compile-time bake-in to the client code.
-     */
-    public static String getAjaxTerm() {
-        return "linux";
-    }
+	/**
+	 * Write to the child process.
+	 */
+	public void write(String k) throws IOException {
+		if (k != null && k.length() != 0) {
+			out.write(k);
+			out.flush();
+		}
+	}
 
-    private static final Logger LOGGER = Logger.getLogger(Session.class.getName());
+	public Process getChildProcess() {
+		return childProcess;
+	}
+
+	/**
+	 * Name of the terminal ajaxterm.js is implmenting.
+	 *
+	 * A static method instead of a constant to avoid compile-time bake-in to the client code.
+	 */
+	public static String getAjaxTerm() {
+		return "linux";
+	}
+
+	private static final Logger LOGGER = Logger.getLogger(Session.class.getName());
+
+	public void kill() {
+		getChildProcess().destroy();
+	}
+
 }
